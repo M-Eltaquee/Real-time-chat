f27dd1ea2a58b5dfa436407aedecde2aa81db622
M-Eltaquee <igs.meltaquee@gmail.com>
2022-08-29 12:33:35 +0400

    3 files modified, 6 added, 1 deleted

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
-import Demo from './demo';
-
-ReactDOM.createRoot(document.querySelector('#root')).render(
-  <StyledEngineProvider injectFirst>
-    <Demo />
-  </StyledEngineProvider>
-);
+import App from './App'
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
