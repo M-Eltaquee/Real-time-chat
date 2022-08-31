8deee6c7c7411c226c2bebc2f178798632a503a7
M-Eltaquee <igs.meltaquee@gmail.com>
2022-08-31 11:14:13 +0400

    3 files modified, 1 added

--- App.js
+++ App.js
@@ -1,125 +1,17 @@
 import * as React from 'react';
-import {
-  Container,
-  Box,
-  CssBaseline,
-  Typography,
-  TextField,
-  Avatar,
-  Button,
-  Paper,
-} from '@mui/material';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-
-import { styled } from '@mui/material/styles';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 const App = () => {
-  const [userName, setUserName] = React.useState({
-    value: '',
-    isValid: false,
-  });
-  const [password, setPassword] = React.useState({
-    value: '',
-    isValid: false,
-  });
-  const userNameInputRef = React.useRef();
-  const passwordInputRef = React.useRef();
-  const enterUserNameHandler = (e) => {
-    if (userNameInputRef.current.value.length > 0) {
-      return setUserName(() => {
-        return {
-          value: userNameInputRef.current.value,
-          isValid: true,
-        };
-      });
-    } else {
-      return setUserName(() => {
-        return {
-          value: userNameInputRef.current.value,
-          isValid: false,
-        };
-      });
-    }
-  };
-  const enterPasswordHandler = () => {
-    if (passwordInputRef.current.value.length > 5) {
-      return setPassword(() => {
-        return {
-          value: passwordInputRef.current.value,
-          isValid: true,
-        };
-      });
-    } else {
-      return setPassword(() => {
-        return {
-          value: passwordInputRef.current.value,
-          isValid: false,
-        };
-      });
-    }
-  };
-  const formSubmitHandler = (e) => {
-    e.preventDefault();
-    const loginData = {
-      userName: userName.value,
-      password: password.value,
-    };
-    console.log(loginData);
-  };
-  const isFormValid = userName.isValid && password.isValid;
+  const [user, setUser] = React.useState(null);
+  const [isLogged, setIslogged] = React.useState(false);
+  console.log(user, isLogged);
   return (
-    <Container className="app" component="main" maxWidth="xs">
-      <CssBaseline />
-      <Paper
-        sx={{
-          marginTop: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ backgroundColor: 'primary.main', mb: 1 }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography variant="h6">Sign in</Typography>
-        <Box
-          onSubmit={formSubmitHandler}
-          component="form"
-          sx={{ mt: 1 }}
-          onSubmit={formSubmitHandler}
-        >
-          <TextField
-            variant="outlined"
-            label="Your Name"
-            size="small"
-            fullWidth
-            required
-            inputProps={{ ref: userNameInputRef }}
-            onChange={enterUserNameHandler}
-          />
-          <TextField
-            variant="outlined"
-            label="Your Password"
-            fullWidth
-            margin="normal"
-            size="small"
-            type="password"
-            required
-            inputProps={{
-              ref: passwordInputRef,
-            }}
-            onChange={enterPasswordHandler}
-          />
-          <Button
-            type="submite"
-            variant="contained"
-            disabled={!isFormValid}
-            fullWidth
-          >
-            Submit
-          </Button>
-        </Box>
-      </Paper>
-    </Container>
+    <div>
+      <Routes>
+        <Route path="login" component={<LoginPage />} />
+      </Routes>
+      <LoginPage />
+    </div>
   );
 };
 
--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
-ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+const theme = createTheme();
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+);
