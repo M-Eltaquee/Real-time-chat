77a51ecc581fcdfe7c25a727c49c08e3e965ea69
M-Eltaquee <igs.meltaquee@gmail.com>
2022-08-31 12:41:45 +0400

    2 files modified, 1 added

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 const App = () => {
   const [user, setUser] = React.useState(null);
   const [isLogged, setIslogged] = React.useState(false);
   console.log(user, isLogged);
   return (
-    <div>
-      <Routes>
-        <Route path="login" component={<LoginPage />} />
-      </Routes>
-      <LoginPage />
-    </div>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route
+          path="login"
+          component={<LoginPage setUser={setUser} setIslogged={setIslogged} />}
+        />
+      </Route>
+    </Routes>
   );
 };
 
